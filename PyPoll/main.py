5af3717af10143f96679916e3841f3f835fe11c4
unknown <shidalgo1212@gmail.com>
2020-05-16 19:35:16 -0500

    README

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import operator
 
 # Path to collect data from the PyBank folder
 poll_data = os.path.join('PyPoll', 'election_data.csv')
@@ -16,7 +17,7 @@ with open(poll_data, 'r', newline="") as csvfile:
     Candidate_Counts = {}
     Percentage_of_Voters = {}
     
-    # Calculate Total Voters - COMPLETE
+    # Calculate Total Voters
     for row in data:
         Total_Voters = Total_Voters + 1
         Name = row[2]
@@ -25,11 +26,22 @@ with open(poll_data, 'r', newline="") as csvfile:
         if Name not in Candidate_Counts:
             Candidate_Counts[Name] = 0
         Candidate_Counts[Name]= Candidate_Counts[Name] + 1
-        Percentage_of_Voters[Name] = round(((Candidate_Counts[Name]/ Total_Voters) * 100),3)
+        Percentage_of_Voters[Name] = round((Candidate_Counts[Name]/ Total_Voters)*100,2)
          
     #Identify the Winner of the Election
-        # if Candidate_Counts[Name]>Candidate_Counts:
-        #     Winner = Name
+    Winner = max(Candidate_Counts, key=Candidate_Counts.get)
+    # print(Winner)
+
+    # Combine Percentage_of_Voters and Candidate_Counts dictionaries by key
+    result = {}
+    for key in (Percentage_of_Voters.keys() | Candidate_Counts.keys()):
+        if key in Percentage_of_Voters: result.setdefault(key, []).append(Percentage_of_Voters[key])
+        if key in Candidate_Counts: result.setdefault(key, []).append(Candidate_Counts[key])
+    # print(result)
+
+    # Sort combined dictionaries by value in descending order
+    sorted_result = dict(sorted(result.items(), key=operator.itemgetter(1),reverse=True))
+    # print(sorted_result)
 
 #Print Election Results
 print("Election Results")
@@ -37,15 +49,11 @@ print("-------------------------")
 #Print Total Voters
 print(f"Total Voters: {Total_Voters}")
 print("-------------------------")
-print(Percentage_of_Voters)
-    # Iterate over key/value pairs in dict and print them
-    # for key, value in Percentage_of_Voters.items():
-        # print(key, ' : ', value)
-print(Candidate_Counts)
+for key in sorted_result:
+    print(key, ':', sorted_result[key])
 print("-------------------------")
-# Print Winner
-print("Winner: Khan")
+print(f"Winner: {Winner}")
 print("-------------------------")
 
-# Create Text File
-f= open("PyPoll_Text_File","w")
+# # Create Text File
+# f= open("PyPoll_Text_File","w")
